f065e4f5ff43f23527a4b8cd91621451446f172f
mlin <491601759@qq.com>
2021-01-23 14:10:59 +0000

    20210122-2210

--- cource_小甲鱼/38 KMP算法之NEXT数组代码原理分析/next.c
+++ cource_小甲鱼/38 KMP算法之NEXT数组代码原理分析/next.c
@@ -1,11 +1,11 @@
-void get_next( String T, int *next )
+void get_next(String T, int *next)
 {
-	j = 0;
-	i = 1;
+	j = 0; //前缀
+	i = 1; //后缀
 	next[1] = 0;
-	while( i < T[0] )
+	while (i < T[0])
 	{
-		if( 0==j || T[i] == T[j] )
+		if (0 == j || T[i] == T[j])
 		{
 			i++;
 			j++;
@@ -16,6 +16,6 @@ void get_next( String T, int *next )
 			j = next[j];
 		}
 	}
-	
+
 	// 因为前缀是固定的，后缀是相对的。
 }
